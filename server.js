559134a277943a9d866476b21d8ed20cabee57b9
Pratham <prathameshgite366@gmail.com>
2024-08-15 16:37:05 +0530

    Task Complete

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/articles");
+const Article = require("./models/article");
+const methodOverride = require("method-override");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/blogApp");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", router);
+
+app.listen(3000);
